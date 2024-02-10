63074a40f00bf884c2764593a6663eff4d82be9b
Philippe Berck <149965800+philippeberck70@users.noreply.github.com>
2024-02-10 17:23:57 +0100

    update3

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
+import image1 from '../../image1.jpg'
 
 const Home = () => {
   const navigate = useNavigate();
   return (
     <div className='row'>
-
-      <div className='col'>
+      <div className='col border p-3'>
         <h3>Exercices de français</h3>
-        <p>Gopb est un site Internet pour tester ses compétences en orthographe.</p>
+        <p><strong>Gopb</strong> est un site Internet pour tester ses compétences en orthographe.</p>
         <button onClick={() => navigate("/exercices")} className='btn btn-secondary'>Commencer</button>
       </div>
       <div className='col'>
-        <img src="image1.jpg" className="rounded float-start" alt="..."></img>
+        <img src={image1} className="rounded float-start" alt="..."></img>
       </div>
 
 
